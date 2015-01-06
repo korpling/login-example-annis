2b59fcc42ab01963012589aec5f867861927d381
Thomas Krause <thomaskrause@posteo.de>
2015-01-06 19:26:03 +0100

    actually submit login information

--- protected/createtempuser.php
+++ protected/createtempuser.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include_once '../common/Config.php';
 include_once '../common/UserCreator.php';
 ?>
@@ -10,20 +10,26 @@ include_once '../common/UserCreator.php';
     <title>Protected login</title>
   </head>
   <body>
-    <p>
-      <?php
-      $annisUser = '';
-      $annisPwd = '';
-      // we expect the REMOTE_USER variable to be set to a valid identifier
-      if (filter_has_var(INPUT_SERVER, 'REMOTE_USER')) {
-        $annisUser = filter_input(INPUT_SERVER, 'REMOTE_USER');
-        // create a new user by using the REST API
-        $creator = new UserCreator();
-        $annisPwd = $creator->createTemporaryUser($annisUser);
-        print('Created user!<br />');
-      }
-      print('Hello World');
-      ?>
-    </p>
+    <?php
+    $annisUser = '';
+    $annisPwd = '';
+    // we expect the REMOTE_USER variable to be set to a valid identifier
+    if (filter_has_var(INPUT_SERVER, 'REMOTE_USER')) {
+      $annisUser = filter_input(INPUT_SERVER, 'REMOTE_USER');
+      // create a new user by using the REST API
+      $creator = new UserCreator();
+      $annisPwd = $creator->createTemporaryUser($annisUser);
+    }
+    ?>
+    <form action="/annis-gui/login" id="loginForm" method="POST">
+      <input type="hidden" name="annis-login-user" value="<?=$annisUser?>">
+      <input type="hidden" name="annis-login-password" value="<?=$annisPwd?>">
+    </form>
+    <p>Automatically logging in user &quot;<?=$annisUser?>&quot;</p>
+    <script>
+      $(document).ready(function () {
+        $("#loginForm").submit();
+      });
+    </script>
   </body>
 </html>
